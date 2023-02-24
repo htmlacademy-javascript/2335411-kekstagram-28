6c862c45e5b4c5cd182ef8bce343fdc77da460f9
detishluh <detishluhh@gmail.com>
2023-02-24 22:23:29 +0500

    +Функции дз

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,4 @@
-// Построить функцию для проверки длины строки, она должна принимать строку(string) и максимальную длину(length)
-// возвращать true если string <= length, false > length
-
-// // // Cтрока короче 20 символов
-// // имяФункции('проверяемая строка', 20); // true
-// // // Длина строки ровно 18 символов
-// // имяФункции('проверяемая строка', 18); // true
-// // // Строка длиннее 10 символов
-// // имяФункции('проверяемая строка', 10); // false
-
+//Функция проверки длины строки
 const getCount = (string, length) => {
   if (string.length <= length) {
     return true;
@@ -15,20 +6,36 @@ const getCount = (string, length) => {
     return false;
   }
 };
-getCount('проверка слова', 10);
-
-// Построить function для проверки палиндрома
 
 
+// Функция проверки палиндрома
 const isPalindrome = (string) => {
-  const tempString = string
-    .toLowerCase()
-    .replaceAll(' ', '');
+  const tempString = string.toLowerCase().replaceAll(' ', '');
   let reverseString = '';
   for (let i = tempString.length - 1; i >= 0; i--) {
     reverseString += tempString.at(i);
   }
   return tempString === reverseString;
-
 };
 
+
+// Функция извлекает цифры из строки
+const getNum = (string) => {
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+      result += string.at(i);
+    }
+  }
+  return parseInt(result, 10);
+};
+
+
+// Функция принимает три параметра
+const getPadStart = (string, minLength, pad) => {
+  const actualPad = minLength - string.length;
+  if (actualPad <= 0) {
+    return string;
+  }
+  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string;
+};
